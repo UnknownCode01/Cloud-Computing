bda83ec1ec4f98adfe51640fec5e9bbf28d06f2b
Brijit Adak <brijitadak000@gmail.com>
2024-02-06 02:09:58 +0530

    add

--- ASS1/Day1/RR_d.java
+++ ASS1/Day1/RR_d.java
@@ -1,142 +1,127 @@
 import java.util.*;
 
-public class RR_d{
-	private static Scanner inp = new Scanner(System.in);
-	//Driver Code
-	public static void main(String[] args){
-		int n,tq, timer = 0, maxProccessIndex = 0;
-		float avgWait = 0, avgTT = 0;
-		System.out.print("\nEnter the time quanta : ");
-		tq = inp.nextInt();
-		System.out.print("\nEnter the number of processes : ");
-		n = inp.nextInt();
-		int arrival[] = new int[n];
-		int burst[] = new int[n];
-		int wait[] = new int[n];
-		int turn[] = new int[n];
-		int queue[] = new int[n];
-		int temp_burst[] = new int[n];
-		boolean complete[] = new boolean[n];
+public class RR_d {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of processes: ");
+        int n = sc.nextInt();
+        System.out.print("Enter the time quantum: ");
+        int tq = sc.nextInt();
+        int pid[] = new int[n];
+        int bt[] = new int[n];
+        int temp_bt[] = new int[n];
+        int at[] = new int[n];
+        int complete[] = new int[n];
+        Queue<Integer> q = new LinkedList<>();  //q- > Queue
+        Queue<Integer> gc = new LinkedList<>(); //gc -> Gantt Chart
+        Iterator iterator = q.iterator();
 
-		System.out.print("\nEnter the arrival time of the processes : ");
-		for(int i = 0; i < n; i++)
-			arrival[i] = inp.nextInt();
+        for (int i = 0; i < n; i++) {
+            pid[i] = i;
+        }
 
-		System.out.print("\nEnter the burst time of the processes : ");
-		for(int i = 0; i < n; i++){
-			burst[i] = inp.nextInt();
-			temp_burst[i] = burst[i];
-		}
+        for (int i = 0; i < n; i++) {   //setting all as not completed
+            complete[i] = 0;
+        }
 
-		for(int i = 0; i < n; i++){ //Initializing the queue and complete array
-			complete[i] = false;
-			queue[i] = 0;
-		}
-		while(timer < arrival[0]) //Incrementing Timer until the first process arrives
+        System.out.println("Enter the Burst Time for each process.");
+        for (int i = 0; i < n; i++) {
+            System.out.print("For Process " + (i + 1) + ":");
+            bt[i] = sc.nextInt();
+            temp_bt[i] = bt[i];
+        }
+
+        System.out.println("Enter the arrival time for each process.");
+        for (int j = 0; j < n; j++) {
+            System.out.print("For Process " + (j + 1) + ":");
+            at[j] = sc.nextInt();
+        }
+
+        int wt[] = new int[n];
+        int tt[] = new int[n];  
+        int ct[] = new int[n];  
+        int p = 0;  //p points the lowest arrival time
+        int pval = 99999;  
+        int added_q[] = new int[n];
+        for(int i=0;i<n;i++){   //Initializing empty added q
+            added_q[i] = 0;
+        }
+        for(int i=0;i<n;i++){
+            if(at[i]<pval){
+                pval=at[i];
+                p=i;
+            }
+        }
+        int timer = 0;
+        while(timer < at[p]) //Incrementing Timer until the first process arrives
 			timer++; 
-		queue[0] = 1;
-		
-		while(true){
-			boolean flag = true;
-			for(int i = 0; i < n; i++){
-				if(temp_burst[i] != 0){
-					flag = false;
-					break;
-				}
-			}
-			if(flag)
-				break;
 
-			for(int i = 0; (i < n) && (queue[i] != 0); i++){
-				int ctr = 0;
-				while((ctr < tq) && (temp_burst[queue[0]-1] > 0)){
-					temp_burst[queue[0]-1] -= 1;
-					timer += 1;
-					ctr++;
+        q.add(pid[p]);
+        added_q[p] = 1;
+        
+        while(iterator.hasNext()){
+            int k=0;
+            int temp_name = q.poll();
+            int tn = temp_name;
+            tn++;
+            gc.add(tn);
+            while(pid[k]!=temp_name){
+                k++;
+            }
+            if(bt[k]>tq){
+                bt[k]-=tq;
+                timer+=tq;
+            }
+            else
+            {
+                timer+=bt[k];
+                bt[k] = 0;
+                complete[k] = 1;
+                ct[k] = timer;
+                tt[k] = ct[k]-at[k];
+                wt[k] = tt[k]-temp_bt[k];
+            }
+ 
+            while(true){        //it checks ay other process has arived at the time
+            pval=timer;
+            int flag = 0;
+                for(int i=0;i<n;i++){
+                    if(at[i] <= pval && complete[i] == 0 && added_q[i] == 0){
+                        pval=at[i];
+                        p=i;
+                        flag=1;
+                     }
+                }
+                if(flag==1){
+                    q.add(p);
+                    added_q[p] = 1;
+                }
+                if(flag!=1){
+                    break;
+                }
+            }
+            if(complete[k]==0){
+                q.add(pid[k]);
+            }
+        }
 
-					//Updating the ready queue until all the processes arrive
-					checkNewArrival(timer, arrival, n, maxProccessIndex, queue);
-				}
-				if((temp_burst[queue[0]-1] == 0) && (complete[queue[0]-1] == false)){
-					turn[queue[0]-1] = timer;	 //turn currently stores exit times
-					complete[queue[0]-1] = true;
-				}
-				
-				//checks whether or not CPU is idle
-				boolean idle = true;
-				if(queue[n-1] == 0){
-					for(int k = 0; k < n && queue[k] != 0; k++){
-						if(complete[queue[k]-1] == false){
-							idle = false;
-						}
-					}
-				}
-				else
-					idle = false;
+        // Printing 
+        System.out.println("\nProcesses ||" + " Burst Time ||" + " Arrival Time ||" + " Completion Time ||"  + " Turn-Around Time  ||" + " Waiting Time ");
+        float awt = 0;
+        float att = 0;
+        for (int i = 0; i < n; i++) {
+            awt = awt + wt[i];
+            att = att + tt[i];
+            System.out.println("   " + (pid[i]+1) + "\t  ||\t" + temp_bt[i] + "\t||\t" + at[i] + "\t||\t  " + ct[i] + "\t   ||\t   " + tt[i] + "\t\t||\t " + wt[i]);
+        }
 
-				if(idle){
-					timer++;
-					checkNewArrival(timer, arrival, n, maxProccessIndex, queue);
-				}
-			
-				//Maintaining the entries of processes after each premption in the ready Queue
-				queueMaintainence(queue,n);
-			}
-		}
+        awt = awt / n;
+        att = att / n;
 
-		for(int i = 0; i < n; i++){
-			turn[i] = turn[i] - arrival[i];
-			wait[i] = turn[i] - burst[i];
-		}
+        System.out.println("\nAverage waiting time = " + awt);
+        System.out.println("\nAverage turn around time = " + att);
 
-		System.out.print("\nProgram No.\tArrival Time\tBurst Time\tWait Time\tTurnAround Time"
-						+ "\n");
-		for(int i = 0; i < n; i++){
-			System.out.print(i+1+"\t\t"+arrival[i]+"\t\t"+burst[i]
-							+"\t\t"+wait[i]+"\t\t"+turn[i]+ "\n");
-		}
-		for(int i =0; i< n; i++){
-			avgWait += wait[i];
-			avgTT += turn[i]; 
-		}
-		System.out.print("\nAverage wait time : "+(avgWait/n)
-						+"\nAverage Turn Around Time : "+(avgTT/n));
-	}
-	public static void queueUpdation(int queue[],int timer,int arrival[],int n, int maxProccessIndex){
-		int zeroIndex = -1;
-		for(int i = 0; i < n; i++){
-			if(queue[i] == 0){
-				zeroIndex = i;
-				break;
-			}
-		}
-		if(zeroIndex == -1)
-			return;
-		queue[zeroIndex] = maxProccessIndex + 1;
-	}
-
-	public static void checkNewArrival(int timer, int arrival[], int n, int maxProccessIndex,int queue[]){
-		if(timer <= arrival[n-1]){
-			boolean newArrival = false;
-			for(int j = (maxProccessIndex+1); j < n; j++){
-				if(arrival[j] <= timer){
-					if(maxProccessIndex < j){
-						maxProccessIndex = j;
-						newArrival = true;
-					}
-				}
-			}
-			if(newArrival) //adds the index of the arriving process(if any)
-				queueUpdation(queue,timer,arrival,n, maxProccessIndex);	 
-		}
-	}
-
-	public static void queueMaintainence(int queue[], int n){
-
-		for(int i = 0; (i < n-1) && (queue[i+1] != 0) ; i++){
-			int temp = queue[i];
-			queue[i] = queue[i+1];
-			queue[i+1] = temp; 
-		}
-	}
+        System.out.print("GC: "+gc);
+        
+    }
 }
---
+++ ASS1/Day1/SJF.java
@@ -0,0 +1,146 @@
+import java.io.*;
+import java.util.*;
+
+public class SJF {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of processes: ");
+        int n = sc.nextInt();
+        int pid[] = new int[n];
+        int bt[] = new int[n];
+        int at[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            pid[i] = (i+1);
+        }
+
+        System.out.println("Enter the Burst Time for each process.");
+        for (int i = 0; i < n; i++) {
+            System.out.print("For Process " + (i + 1) + ":");
+            bt[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the arrival time for each process.");
+        for (int j = 0; j < n; j++) {
+            System.out.print("For Process " + (j + 1) + ":");
+            at[j] = sc.nextInt();
+        }
+
+        //Bubble sort on arrival time
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(at[j]>at[j+1]){
+                    int t1,t2,t3;
+                    t1=pid[j];
+                    pid[j]=pid[j+1];
+                    pid[j+1]=t1;
+
+                    t2=at[j];
+                    at[j]=at[j+1];
+                    at[j+1]=t2;
+
+                    t3=bt[j];
+                    bt[j]=bt[j+1];
+                    bt[j+1]=t3;
+                }
+            }
+        }
+        //Bubble sort on burst time
+        for(int k=0;k<n;){
+            int i,j;
+            i=k;j=k;
+            while(i==j && j<=k){
+                j++;
+            }
+            j--;
+            for(int p=i;p<j;p++){
+                for(int q=i;q<j-1;q++){
+                if(bt[q]>bt[q+1]){
+                    int t1,t2,t3;
+                    t1=pid[q];
+                    pid[q]=pid[q+1];
+                    pid[q+1]=t1;
+
+                    t2=at[q];
+                    at[q]=at[q+1];
+                    at[q+1]=t2;
+
+                    t3=bt[q];
+                    bt[q]=bt[q+1];
+                    bt[q+1]=t3;
+                }
+                }
+            }
+            k=j+1;
+        }
+
+		// main algo
+        int wt[] = new int[n];
+        int tt[] = new int[n];  
+        int ct[] = new int[n+1];
+
+        // calculation of tat ct and wt ; ct[i+1]--> ct of i (exception)
+        int id=0;   // id-> idle time
+        ct[0]=0;
+        int ptr=0;  // ptr is to find out after ct which are the programs present
+        if(at[0]>ct[0]){
+            id=at[0]-ct[0];
+        }
+        for(int i=0;i<n;i++){
+                ct[i+1]=ct[i]+bt[i]+id;
+                tt[i]=ct[i+1]-at[i];
+                wt[i]=tt[i]-bt[i];
+                boolean t = false;
+                id=0;
+                for(int j=i+1;j<n;j++){
+                    if(at[j]<=ct[i+1]){
+                        ptr=j;
+                        t=true;
+                    }
+                    else if(at[j]>ct[i+1] && t!=true){
+                        id++;
+                        break;
+                    }
+                    else if(at[j]>ct[i+1]){
+                        break;
+                    }
+                }
+                for(int p=i+1;p<=ptr;p++){
+                for(int q=i+1;q<=ptr-1;q++){
+                if(bt[q]>bt[q+1]){
+                    int t1,t2,t3;
+                    t1=pid[q];
+                    pid[q]=pid[q+1];
+                    pid[q+1]=t1;
+
+                    t2=at[q];
+                    at[q]=at[q+1];
+                    at[q+1]=t2;
+
+                    t3=bt[q];
+                    bt[q]=bt[q+1];
+                    bt[q+1]=t3;
+                }
+                }
+                }
+            }
+            
+        System.out.println("\nProcesses ||" + " Burst Time ||" + " Arrival Time ||" + " Completion Time ||"  + " Turn-Around Time  ||" + " Waiting Time ");
+        float awt = 0;
+        float att = 0;
+        for (int i = 0; i < n; i++) {
+            awt = awt + wt[i];
+            att = att + tt[i];
+            System.out.println("   " + pid[i] + "\t  ||\t" + bt[i] + "\t||\t" + at[i] + "\t||\t  " + ct[i+1] + "\t   ||\t   " + tt[i] + "\t\t||\t " + wt[i]);
+        }
+
+        awt = awt / n;
+        att = att / n;
+
+        System.out.println("\nAverage waiting time = " + awt);
+        System.out.println("\nAverage turn around time = " + att);
+    }
+
+}
+
